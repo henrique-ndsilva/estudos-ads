5d688f1039226bdab02d478a72862f7ed0599bed
Henrique Nunes <ownerandpitica@gmail.com>
2025-06-05 07:31:09 -0300

    refatoração do código.

---
+++ js/functions.js
@@ -0,0 +1,34 @@
+function toggleFontSize() {
+    const fontbutton = document.getElementById("fontbutton");
+    let body = document.body;
+    let tamanhoAtual = window.getComputedStyle(body).fontSize;
+
+    if (tamanhoAtual === "12px") {
+        body.style.fontSize = "14px";
+        fontbutton.textContent = "Diminuir Fonte"
+    } else {
+        body.style.fontSize = "12px";
+        fontbutton.textContent = "Aumentar Fonte"
+    }
+}
+
+function toggleTrimestre(btnId, divId) {
+    const btn = document.getElementById(btnId);
+    const div = document.getElementById(divId);
+
+    div.style.display = "none";
+    btn.textContent = `Mostrar ${btn.dataset.label}`;
+
+    btn.addEventListener("click", () => {
+        if (div.style.display === "none" || div.style.display === "") {
+            div.style.display = "block";
+            btn.textContent = `Ocultar ${btn.dataset.label}`;
+        } else {
+            div.style.display = "none";
+            btn.textContent = `Mostrar ${btn.dataset.label}`;
+        }
+    });
+}
+
+toggleTrimestre("btn-trimestre1", "trimestre1");
+toggleTrimestre("btn-trimestre2", "trimestre2");
