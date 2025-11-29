fcf1e2429e9012469e3d354d448598c306d89a3e
Henrique Nunes <henrique.ndslv@gmail.com>
2025-11-29 00:16:57 -0300

    4º Trimestre to-do

--- js/functions.js
+++ js/functions.js
@@ -1,35 +1,36 @@
 function toggleFontSize() {
-    const fontbutton = document.getElementById("fontbutton");
-    let body = document.body;
-    let tamanhoAtual = window.getComputedStyle(body).fontSize;
+  const fontbutton = document.getElementById("fontbutton");
+  let body = document.body;
+  let tamanhoAtual = window.getComputedStyle(body).fontSize;
 
-    if (tamanhoAtual === "12px") {
-        body.style.fontSize = "14px";
-        fontbutton.textContent = "Diminuir Fonte"
-    } else {
-        body.style.fontSize = "12px";
-        fontbutton.textContent = "Aumentar Fonte"
-    }
+  if (tamanhoAtual === "12px") {
+    body.style.fontSize = "14px";
+    fontbutton.textContent = "Diminuir Fonte";
+  } else {
+    body.style.fontSize = "12px";
+    fontbutton.textContent = "Aumentar Fonte";
+  }
 }
 
 function toggleTrimestre(btnId, divId) {
-    const btn = document.getElementById(btnId);
-    const div = document.getElementById(divId);
+  const btn = document.getElementById(btnId);
+  const div = document.getElementById(divId);
 
-    div.style.display = "none";
-    btn.textContent = `Mostrar ${btn.dataset.label}`;
+  div.style.display = "none";
+  btn.textContent = `Mostrar ${btn.dataset.label}`;
 
-    btn.addEventListener("click", () => {
-        if (div.style.display === "none" || div.style.display === "") {
-            div.style.display = "block";
-            btn.textContent = `Ocultar ${btn.dataset.label}`;
-        } else {
-            div.style.display = "none";
-            btn.textContent = `Mostrar ${btn.dataset.label}`;
-        }
-    });
+  btn.addEventListener("click", () => {
+    if (div.style.display === "none" || div.style.display === "") {
+      div.style.display = "block";
+      btn.textContent = `Ocultar ${btn.dataset.label}`;
+    } else {
+      div.style.display = "none";
+      btn.textContent = `Mostrar ${btn.dataset.label}`;
+    }
+  });
 }
 
 toggleTrimestre("btn-trimestre1", "trimestre1");
 toggleTrimestre("btn-trimestre2", "trimestre2");
 toggleTrimestre("btn-trimestre3", "trimestre3");
+toggleTrimestre("btn-trimestre4", "trimestre4");
